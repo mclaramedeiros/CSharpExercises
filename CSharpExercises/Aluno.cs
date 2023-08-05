d6a5f0b4bce674cddc61fae5b936dbe2d079bbfa
mclaramedeiros <claramedeiroswon@gmail.com>
2023-08-05 14:56:46 -0300

    add logic Aluno

---
+++ CSharpExercises/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public double NotaPrimeiroBimestre { get; set;}
+        public double NotaSegundoBimestre { get; set;}
+        public double NotaTerceiroBimestre { get; set; }
+
+        public double NotaFinal() 
+        { 
+            return NotaPrimeiroBimestre + NotaSegundoBimestre + NotaTerceiroBimestre;
+        }
+        
+        public bool Aprovado()
+        {
+            double notafinal = NotaFinal();
+            if(notafinal >= 60)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public double NotaPendente()
+        {
+            double notafinal = NotaFinal();
+            if(notafinal >= 60)
+            {
+                return 0.0;
+            }
+            return 60 - notafinal;
+        }
+    }
+}
--- CSharpExercises/Program.cs
+++ CSharpExercises/Program.cs
@@ -18,18 +18,38 @@ namespace CSharpExercises
             //Console.WriteLine("PERIMETRO = " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
             //Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
 
-            Funcionario funcionario = new Funcionario();
-            Console.Write("Nome: ");
-            funcionario.Name = Console.ReadLine();
-            Console.Write("Salario bruto: ");
-            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Imposto: ");
-            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Dados do funcionário: " + funcionario.Name, funcionario.SalarioLiquido);
-            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
-            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            funcionario.AumentarSalario(porcentagem);
-            Console.WriteLine("Dados atualizados " + funcionario);
+            /// Lógica Funcionário
+            //Funcionario funcionario = new Funcionario();
+            //Console.Write("Nome: ");
+            //funcionario.Name = Console.ReadLine();
+            //Console.Write("Salario bruto: ");
+            //funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.Write("Imposto: ");
+            //funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Dados do funcionário: " + funcionario.Name, funcionario.SalarioLiquido);
+            //Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            //double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //funcionario.AumentarSalario(porcentagem);
+            //Console.WriteLine("Dados atualizados " + funcionario);
+
+            ///Lógica Aluno
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Nome do aluno");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno");
+            aluno.NotaPrimeiroBimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaSegundoBimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaTerceiroBimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            } else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("Faltaram " + aluno.NotaPendente().ToString("F2", CultureInfo.InvariantCulture))
+            }
         }
     }
 }
