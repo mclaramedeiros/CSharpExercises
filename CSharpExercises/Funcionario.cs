0004998d83f1eae682b3f0775aae5a44c172bcc5
mclaramedeiros <claramedeiroswon@gmail.com>
2023-08-05 14:38:34 -0300

    add funcionario class

---
+++ CSharpExercises/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    class Funcionario
+    {
+        public string Name { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Name 
+            + ", R$ " 
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- CSharpExercises/Program.cs
+++ CSharpExercises/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 
 namespace CSharpExercises
@@ -7,15 +8,28 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            Retangulo retangulo = new Retangulo();
-            Console.Write("Entre com a altura e largura do retângulo");
-            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        
-            Console.WriteLine("AREA = " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("PERIMETRO = " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
-              
+            /// Lógica Retângulo
+            //Retangulo retangulo = new Retangulo();
+            //Console.Write("Entre com a altura e largura do retângulo");
+            //retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.WriteLine("AREA = " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine("PERIMETRO = " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Nome: ");
+            funcionario.Name = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados do funcionário: " + funcionario.Name, funcionario.SalarioLiquido);
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+            Console.WriteLine("Dados atualizados " + funcionario);
         }
     }
 }
